35185170e5d5603ff4889c0fbf09a8797b5fbf59
Varun Jadon <varunjadon.se@gmail.com>
2023-03-07 21:02:07 +0530

    Product Reducer &Action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,13 +1,17 @@
-export const productListReducer = (state = {products : []}, action) => {
-    switch (action.type) {
-        case 'PRODUCT_LIST_REQUEST':
-            return { loading: true , products :[]}
-        
-        case 'PRODUCT_LIST_SUCESS':
-            return { loading : false , products : action.payload}
-        case 'PRODUCT_LIST_FAIL':
-            return { loading: false, error: action.payload }
-        default:
-            return state 
-    }
-}
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_REQUEST,
+} from "../constants/productConstant";
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] };
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
