0fb9798973b8e1a68e6526c9dc4f56c3c15448db
Varun Jadon <varunjadon.se@gmail.com>
2023-03-08 01:11:10 +0530

    Product Details Reducer and Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,13 @@ import { legacy_createStore as createStore } from 'redux'
 // import { configureStore } from "@reduxjs/toolkit";
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer} from './reducers/productReducers'
+import { productListReducer,productDetailsReducer} from './reducers/productReducers'
 
 
 const reducer = combineReducers({
-    productList : productListReducer,
-})
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
 
 const initialState = {}
 
